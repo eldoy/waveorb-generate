f4c52c5f4a13448b2be5c21e3e2d7bc17c4daa52
Vidar Eldøy <vidar@eldoy.com>
2021-02-14 09:30:44 +0100

    Update generate

--- templates/pages/list.js
+++ templates/pages/list.js
@@ -1,30 +1,28 @@
 module.exports = async function($) {
-  $.page.title = '__Name__s'
+  $.page.title = '__Names__'
 
   async function renderList() {
     var items = await api({ action: '__name__/find' })
     if (items.length) {
       html('#list', /* html */`
-        <ul>
-          ${items.map(item => {
-            return /* html */`
-              <li>
-                ${esc(item.name)}
-                <nav>
-                  <a href="${$.link(`__name__/show?__name___id=${item.id}`)}">
-                    Show
-                  </a>
-                  <a href="${$.link(`__name__/edit?__name___id=${item.id}`)}">
-                    Edit
-                  </a>
-                  <a href="${$.link(`__name__/delete?__name___id=${item.id}`)}">
-                    Delete
-                  </a>
-                </nav>
-              </li>
-            `
-          }).join('')}
-        </ul>
+        ${items.map(item => {
+          return /* html */`
+            <div class="flex list">
+              ${esc(item.name)}
+              <nav>
+                <a href="${$.link(`__name__/show?__name___id=${item.id}`)}">
+                  Show
+                </a>
+                <a href="${$.link(`__name__/edit?__name___id=${item.id}`)}">
+                  Edit
+                </a>
+                <a href="${$.link(`__name__/delete?__name___id=${item.id}`)}">
+                  Delete
+                </a>
+              </nav>
+            </div>
+          `
+        }).join('')}
       `)
     } else {
       html('#list', `No __names__ found... Click on <mark>&ldquo;New __name__&rdquo;</mark> to create one!`)
@@ -32,8 +30,8 @@ module.exports = async function($) {
   }
 
   return /* html */`
-    <div class="title">
-      <h1>__Name__s</h1>
+    <div class="flex">
+      <h1>__Names__</h1>
       <a href="${$.link('__name__/new')}">New __name__</a>
     </div>
     <div id="list"></div>
