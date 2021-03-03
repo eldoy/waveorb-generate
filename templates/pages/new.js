30e146872f4d3a2776272c2e033613b53bbb86b4
Vidar Eldøy <vidar@eldoy.com>
2021-03-03 09:31:52 +0100

    Add radio, checkbox and select to forms

--- templates/pages/new.js
+++ templates/pages/new.js
@@ -1,3 +1,7 @@
+function cap(str) {
+  return (str[0].toUpperCase() + str.slice(1)).replace(/[-_]/g, ' ')
+}
+
 module.exports = function({ base, fields, name, Name }) {
   const entries = Object.entries(fields)
 
@@ -14,17 +18,41 @@ module.exports = function({ base, fields, name, Name }) {
   function inputs() {
     if (entries.length) {
       return `${entries.map(([k, v]) => {
-        return `      <p>
-        <label for="${k}">${k[0].toUpperCase() + k.slice(1)}</label>
+        const [type, ...options] = v.split('.')
+        const fieldset = ['radio', 'check', 'checkbox'].includes(type)
+        return `      <${fieldset ? 'fieldset' : 'p'}>
+        ${fieldset ? `<legend>${cap(k)}</legend>` : `<label for="${k}">${cap(k)}</label>`}
         ${function() {
-          if (v == 'text') {
+          if (type == 'text') {
             return `<textarea id="${k}" name="${k}" oninput="clearErrors(this)"></textarea>`
+          } else if (type == 'select') {
+            if (options.length) {
+              return `<select name="${k}">\n${options.map(o => {
+                return `          <option value="${o}">${cap(o)}</option>`
+              }).join('\n')}
+        </select>`
+            }
+            return ''
+          } else if (type == 'radio') {
+            if (options.length) {
+              return options.map(o => {
+                return `<label>${cap(o)} <input type="radio" name="${type}" value="${o}"><label>`
+              }).join('\n        ')
+            }
+            return ''
+          } else if (type == 'checkbox' || type == 'check') {
+            if (options.length) {
+              return options.map(o => {
+                return `<label>${cap(o)} <input type="checkbox" name="${type}" value="${o}"><label>`
+              }).join('\n        ')
+            }
+            return ''
           } else {
-            return `<input id="${k}" type="${v == 'string' ? 'text' : v}" name="${k}" oninput="clearErrors(this)">`
+            return `<input id="${k}" type="${v == 'string' ? 'text' : type}" name="${k}" oninput="clearErrors(this)">`
           }
         }()}
         <em class="${k}-errors"></em>
-      </p>`
+      </${fieldset ? 'fieldset' : 'p'}>`
       }).join('\n')}`
     }
     return ''
