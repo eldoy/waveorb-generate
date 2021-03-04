347b56daa3f880271a1bfcf2e48023049ee15555
Vidar Eldøy <vidar@eldoy.com>
2021-03-04 10:02:42 +0100

    Get rid of trouble function

--- templates/pages/list.js
+++ templates/pages/list.js
@@ -2,14 +2,6 @@ module.exports = function({ base, plural, fields, name, Names }) {
   // Show name, email or id
   const display = fields.name ? 'name' : fields.email ? 'email' : 'id'
 
-  function ids() {
-    const trail = name.split('/').slice(0, -1)
-    if (trail.length) {
-      return `?${trail.map(path => `${path}_id=\${params('${path}_id')}`).join('&')}`
-    }
-    return ''
-  }
-
   return `module.exports = async function($) {
   $.page.title = '${Names}'
 
@@ -44,7 +36,7 @@ module.exports = function({ base, plural, fields, name, Names }) {
   return /* html */\`
     <div class="flex">
       <h1>${Names}</h1>
-      <a href="\${$.link('${base}/new${ids()}')}">New ${base}</a>
+      <a id="new" href="\${$.link('${base}/new')}">New ${base}</a>
     </div>
     <div id="list"></div>
     <script>
